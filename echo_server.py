8f4926591f06662eff88aebcbd638cf6ce282bc2
Humberto Gonzalez <humberto.gonzalezj@gmail.com>
2020-05-20 17:44:58 -0700

    adding in files for assignment 02

--- echo_server.py
+++ echo_server.py
@@ -8,7 +8,7 @@ def server(log_buffer=sys.stderr):
     address = ('127.0.0.1', 10000)
     # TODO: Replace the following line with your code which will instantiate
     #       a TCP socket with IPv4 Addressing, call the socket you make 'sock'
-    sock = None
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
     # TODO: You may find that if you repeatedly run the server script it fails,
     #       claiming that the port is already used.  You can set an option on
     #       your socket that will fix this problem. We DID NOT talk about this
@@ -21,7 +21,8 @@ def server(log_buffer=sys.stderr):
 
     # TODO: bind your new sock 'sock' to the address above and begin to listen
     #       for incoming connections
-
+    sock.bind(address)
+    sock.listen(1)
     try:
         # the outer loop controls the creation of new connection sockets. The
         # server will handle each incoming connection one at a time.
@@ -33,7 +34,7 @@ def server(log_buffer=sys.stderr):
             #       the client so we can report it below.  Replace the
             #       following line with your code. It is only here to prevent
             #       syntax errors
-            conn, addr = ('foo', ('bar', 'baz'))
+            conn, addr = sock.accept()
             try:
                 print('connection - {0}:{1}'.format(*addr), file=log_buffer)
 
@@ -46,14 +47,17 @@ def server(log_buffer=sys.stderr):
                     #       following line with your code.  It's only here as
                     #       a placeholder to prevent an error in string
                     #       formatting
-                    data = b''
+                    data = conn.recv(16)
                     print('received "{0}"'.format(data.decode('utf8')))
                     
                     # TODO: Send the data you received back to the client, log
                     # the fact using the print statement here.  It will help in
                     # debugging problems.
+                    conn.sendall(data)
                     print('sent "{0}"'.format(data.decode('utf8')))
                     
+                    if len(data)<16:
+                        break
                     # TODO: Check here to see whether you have received the end
                     # of the message. If you have, then break from the `while True`
                     # loop.
@@ -66,6 +70,7 @@ def server(log_buffer=sys.stderr):
                 traceback.print_exc()
                 sys.exit(1)
             finally:
+                conn.close()
                 # TODO: When the inner loop exits, this 'finally' clause will
                 #       be hit. Use that opportunity to close the socket you
                 #       created above when a client connected.
